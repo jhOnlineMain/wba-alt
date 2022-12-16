fe32383162067d82212120ec0a8d216465fc5524
jhdev611 <62648319+jhOnlineMain@users.noreply.github.com>
2022-12-16 16:33:52 +0800

    style

--- src/components/hero.js
+++ src/components/hero.js
@@ -60,7 +60,7 @@ return (
         </div>
         <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
           <img
-            className="absolute inset-0 w-full h-full object-cover"
+            className="absolute inset-0 w-full h-full object-cover object-top"
             src={qry.backgroundImage.sourceUrl}
             alt={qry.backgroundImage.altText}
           />
